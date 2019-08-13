6ea384c30ecdcdcbf6c9e4a024bb469219784374
Luis Novoa <luis.matte.novoa@gmail.com>
2019-08-13 11:46:21 -0400

    function for grid creation implemented

--- etchasketch.js
+++ etchasketch.js
@@ -1,7 +1,25 @@
 let main = document.querySelector('.main');
-for (let i = 0; i < 16*16; i++){
-    let gridCell = document.createElement('div');
-    gridCell.classList.add('cell');
-    gridCell.onmouseover = () => gridCell.classList.add('hover');
-    main.appendChild(gridCell);
+let header = document.querySelector('.header');
+let clear = document.createElement('button');
+let br = document.createElement('br');
+
+clear.classList.add('clear');
+clear.textContent = 'Clear sketch';
+//clear.addEventListener('click');
+header.appendChild(clear);
+
+function newGrid(num) {
+    for (let i = 0; i < num*num; i++){
+        let gridCell = document.createElement('div');
+        gridCell.classList.add('cell');
+        gridCell.onmouseover = () => gridCell.classList.add('hover');
+        main.appendChild(gridCell);
+    };
+    main.style.cssText = `grid-template: repeat(${num}, 1fr) / repeat(${num}, 1fr)`;
+};
+
+function newSketch() {
+    prompt('How many squares per side?');
 }
+
+newGrid(16);
